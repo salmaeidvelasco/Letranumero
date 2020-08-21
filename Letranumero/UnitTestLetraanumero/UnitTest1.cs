7bdfa1c6ba2fb4863b038880fdf33fe57e60898f
salmaeidvelasco <a2019115131@estudiantes.upsa.edu.bo>
2020-08-21 15:47:19 -0400

    Se agregó un segundo UnitTest con Post.

--- Letranumero/UnitTestLetraanumero/UnitTest1.cs
+++ Letranumero/UnitTestLetraanumero/UnitTest1.cs
@@ -15,11 +15,28 @@ namespace UnitTestLetraanumero
             string esperado = "CINCO"; // Aquí tiene que ser igual a lo que escribí en Conversores 
 
             //Act
-           string resultado = literalController.LiteralGet(numero); //Lo que hay en mi Get, porque LiteralController ya hereda de Conversores.
+            string resultado = literalController.LiteralGet(numero); //Lo que hay en mi Get, porque LiteralController ya hereda de Conversores.
 
-
+           
             //Assert
             Assert.AreEqual(esperado, resultado);
         }
-    }
+        //Segundo UnitTest con Post
+        [TestMethod]
+        public void TestNumeroaLetra2()
+        {
+            //Arrange
+            LiteralController literalController = new LiteralController(); 
+        int numero = 8;
+        string esperado = "OCHO"; 
+
+        //Act
+        string resultado = literalController.LiteralPost(numero); 
+
+
+        //Assert
+        Assert.AreEqual(esperado, resultado);
+        }
+
+}
 }
